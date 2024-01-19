fa7f0cf2ff0357e2693d28a8b57bbb83214568ad
Thomas Crow <contact@thomascrowscience.com>
2024-01-19 15:34:50 +0800

    small changes to the scripts to make them run

--- job-submit-scripts/Na-core-files/mdx.sh
+++ job-submit-scripts/Na-core-files/mdx.sh
@@ -3,7 +3,7 @@
 module load gromacs/2023
 
 run_dir=$(pwd)
-core_dir=$run_dir"/core-files"
+core_dir=$run_dir"/Na-core-files"
 
 echo "Running $core_dir/run_lib.py"
 python3 $core_dir/run_lib.py "min" "gmx" $core_dir $run_dir
--- job-submit-scripts/Na-core-files/runsbatch.sh
+++ job-submit-scripts/Na-core-files/runsbatch.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-run_dir=$(pwd)
+#run_dir=$(pwd)
 
 #Submits first time and stores jobid
 premd1=$(sbatch --parsable premd1.sbatch)
 
 #Submits second time dependant on first job completing successfully
 #Wrap this in a for loop if you want a longer chain of dependancies
-premd1=$(sbatch --parsable premd1.sbatch --dependency=afterok:$premd1)
+#premd1=$(sbatch --parsable premd1.sbatch --dependency=afterok:$premd1)
 
 #runs grompp and makes sbatch
 #prep_premd2=$(sbatch --parsable  --dependency=afterok:$premd1)
